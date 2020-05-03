2932bd7b87d6a97b5d3879a6a851ff3d8057e36e
Richard Medina <ricki9@gmail.com>
2020-05-02 21:02:56 -0700

    updating stripe and updating favicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -5,6 +5,7 @@ import { createStructuredSelector } from 'reselect'
 import { selectCartItems, selectCartTotal } from 'redux/cart/cart.selectors'
 
 import CheckoutItem from 'components/checkout-item/checkout-item.component'
+import StripeButton from 'components/stripe-button/stripe-button.component'
 
 import './checkout.styles.scss'
 
@@ -14,7 +15,7 @@ const HeaderBlock = ({ item }) => (
     </div>
 )
 
-const CheckoutPage = ({ cartItems, cartTotal }) => (
+const CheckoutPage = ({ cartItems, total }) => (
     <div className='checkout-page'>
         <div className='checkout-header'>
             {
@@ -27,16 +28,21 @@ const CheckoutPage = ({ cartItems, cartTotal }) => (
         {
             cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem} />)
         }
-        <div className='total'>
-            <span>Total {cartTotal}</span>
+        <div className='total'>Total {total}</div>
+        <div className='test-warning'>
+            Please use the following test credit card for *payments
+            <br />
+            4242 4242 4242 4242  - Exp 01/21 - CVV: 123
+
         </div>
+        <StripeButton price={total} />
     </div>
 )
 
 
 const mapStateToProps = createStructuredSelector({
     cartItems: selectCartItems,
-    cartTotal: selectCartTotal
+    total: selectCartTotal
 })
 
 export default connect (mapStateToProps) (CheckoutPage)
