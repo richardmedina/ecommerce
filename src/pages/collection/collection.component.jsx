043d83388d34a982f0ffb0bdc59d6ec9ebb36957
Richard Medina <ricki9@gmail.com>
2020-05-06 01:18:19 -0700

    Added Container Pattern

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -7,13 +7,15 @@ import { selectCollection } from 'redux/shop/shop.selectors'
 
 import './collection.styles.scss'
 
-const CollectionPage = ({ collection: { title, items } }) => {
+const CollectionPage = ({ collection }) => {
+  console.log('RENDERING COLLECTION: ', collection)
+  const { title, items } = collection
   return (
     <div className='collection-page'>
       <h2 className='title'>{ title }</h2>
       <div className='items'>
         {
-          items.map(item =>
+          items && items.map(item =>
             <CollectionItem key={item.id} item={item} />
           )
         }
--- src/pages/shoppage/shoppage.component.jsx
+++ src/pages/shoppage/shoppage.component.jsx
@@ -1,72 +1,39 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Route } from 'react-router-dom'
+import { createStructuredSelector } from 'reselect'
 
-import { firestore, convertCollectionsSnapshotToMap } from 'firebase/firebase.utils'
-import { updateCollections } from 'redux/shop/shop.actions'
+import CollectionsOverviewContainer from 'components/collections-overview/collections-overview.container'
+import CollectionPageContainer from 'pages/collection/collection.container'
 
-import WithSpinner from 'components/with-spinner/with-spinner.component'
-
-import CollectionsOverview from 'components/collections-overview/collections-overview.component'
-import CollectionPage from 'pages/collection/collection.component'
-
-const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview)
-const CollectionPageWithSpinner = WithSpinner(CollectionPage) 
+import { fetchCollectionsStartAsync } from 'redux/shop/shop.actions'
 
 class ShopPage extends React.Component {
 
-  state = {
-    loading: true
-  }
-
-  unsubscribeFromSnapshot = null
-
   componentDidMount(){
-    const { updateCollections } = this.props
-    const collectionRef = firestore.collection('collections')
+    const { fetchCollectionsStartAsync } = this.props
 
-    collectionRef.get().then(snapshot => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
-
-      console.log('ShopPage collectionsMap =>', collectionsMap)
-      updateCollections(collectionsMap)
-      this.setState({
-        loading: false
-      })
-    })
-
-    // const response = await fetch('https://firestore.googleapis.com/v1/projects/ecommerce-adf6e/databases/(default)/documents/collections')
-    // const collections = await response.json()
-    // console.log("COLLLLLLS: ", collections)
-    //.then(response => response.json())
-    //.then(collections => console.log("COLS: ", collections))
-    
-    // collectionRef.onSnapshot(snapshot => {
-    //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
-
-    //   console.log('ShopPage collectionsMap =>', collectionsMap)
-    //   updateCollections(collectionsMap)
-    //   this.setState({
-    //     loading: false
-    //   })
-    // })
+    fetchCollectionsStartAsync()
   }
 
   render(){
     const { match } = this.props
-    const { loading } = this.state
-
     return (
       <div className='shop-page'>
-        <Route exact path={`${match.path}`} render={props => <CollectionsOverviewWithSpinner isloading={loading} {...props} />} />
-        <Route path={`${match.path}/:collectionId`} render={props => <CollectionPageWithSpinner isLoading={loading} {...props} />} />
+        <Route
+          exact
+          path={`${match.path}`}
+          component={CollectionsOverviewContainer} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPageContainer} />
       </div>
     )
   }
 }
 
 const mapDispatchToProps = dispatch => ({
-  updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
 })
 
 export default connect(null, mapDispatchToProps)(ShopPage)
